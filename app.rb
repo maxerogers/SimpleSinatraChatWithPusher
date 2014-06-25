859e44905e9b047542fb14c85334e9ada4cc43a2
Max Rogers <maxrogers90@gmail.com>
2014-06-24 22:43:35 -0400

    adf

--- app.rb
+++ app.rb
@@ -4,8 +4,11 @@
 require 'rubygems'
 require 'sinatra'
 require './model.rb'
+require 'pusher'
 
 set :public_folder, Proc.new { File.join(root, "public") }
+Pusher.url = "http://8c0bcee100b50a1d7826:6e64b8424aee60e380dd@api.pusherapp.com/apps/79094"
+
 
 get '/' do 
 	@messages = Message.all
@@ -15,6 +18,9 @@ end
 post '/' do 
 	Message.create(username: params[:username],data: params[:data])
 	@messages = Message.all
+	Pusher['test_channel'].trigger('my_event', {
+		message: params[:data]
+	})
 	erb :index
 end
 
